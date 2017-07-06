81fd9187b782ff9917c3d9f4a5213011bbc1a042
ecmadao <wlec@outlook.com>
2017-07-06 15:04:53 +0800

    WHAT: Add coursera ml class data
    WHY:
    
    * XXXXX
    
    HOW:
    
    * NOTHING TO SAY

---
+++ Notes/ml/coursera/machine-learning-ex2/ex2/costFunction.m
@@ -0,0 +1,44 @@
+function [J, grad] = costFunction(theta, X, y)
+%COSTFUNCTION Compute cost and gradient for logistic regression
+%   J = COSTFUNCTION(theta, X, y) computes the cost of using theta as the
+%   parameter for logistic regression and the gradient of the cost
+%   w.r.t. to the parameters.
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+%
+% Note: grad should have the same dimensions as theta
+%
+
+% Cost(h¦È(x),y)= ?ylog(h¦È(x)) ? (1?y)log(1 ? h¦È(x))
+
+% h¦È(x) = 1 / (1 +e ^ (?¦Èx))
+
+[column, row] = size(X);
+
+theta = theta'
+thetaX = sum(theta .* X, 2);
+hx = sigmoid(thetaX);
+cost = -y .* log(hx) .- (1 .- y ) .* log(1 .- hx);
+
+J = sum(cost) / column;
+
+grad1 = sum((hx .- y) .* X(:, 1)) / column;
+grad2 = sum((hx .- y) .* X(:, 2)) / column;
+grad3 = sum((hx .- y) .* X(:, 3)) / column;
+
+grad = [grad1; grad2; grad3];
+
+% =============================================================
+
+end
